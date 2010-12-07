691bb47c1b6c927c5a64418857e8be94805de611
Jordi Boggiano <j.boggiano@seld.be>
2010-12-07 17:13:59 +0100

    Added PHP/Xml/Yaml mapping drivers

---
+++ lib/Doctrine/ODM/PHPCR/Mapping/Driver/XmlDriver.php
@@ -0,0 +1,149 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the LGPL. For more information, see
+ * <http://www.doctrine-project.org>.
+ */
+
+namespace Doctrine\ODM\PHPCR\Mapping\Driver;
+
+use Doctrine\ODM\PHPCR\Mapping\ClassMetadata;
+use Doctrine\ODM\PHPCR\Mapping\MappingException;
+use SimpleXmlElement;
+
+/**
+ * XmlDriver is a metadata driver that enables mapping through XML files.
+ *
+ * @license     http://www.opensource.org/licenses/lgpl-license.php LGPL
+ * @link        www.doctrine-project.org
+ * @since       1.0
+ * @author      Jonathan H. Wage <jonwage@gmail.com>
+ * @author      Roman Borschel <roman@code-factory.org>
+ */
+class XmlDriver extends AbstractFileDriver
+{
+    /**
+     * The file extension of mapping documents.
+     *
+     * @var string
+     */
+    protected $fileExtension = '.dcm.xml';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function loadMetadataForClass($className, ClassMetadata $class)
+    {
+        $xmlRoot = $this->getElement($className);
+        if (!$xmlRoot) {
+            return;
+        }
+
+        if ($xmlRoot->getName() == 'document') {
+            if (isset($xmlRoot['repository-class'])) {
+                $class->setCustomRepositoryClass((string) $xmlRoot['repository-class']);
+            }
+            if (!isset($xmlRoot['alias'])) {
+                throw MappingException::aliasIsNotSpecified($className);
+            }
+            $class->setAlias((string) $xmlRoot['alias']);
+            $class->setNodeType(isset($xmlRoot['nodeType']) ? (string) $xmlRoot['nodeType'] : 'nt:unstructured');
+        } elseif ($xmlRoot->getName() === 'mapped-superclass') {
+            $class->isMappedSuperclass = true;
+        }
+
+        if (isset($xmlRoot->field)) {
+            foreach ($xmlRoot->field as $field) {
+                $mapping = array();
+                $attributes = $field->attributes();
+                foreach ($attributes as $key => $value) {
+                    $mapping[$key] = (string) $value;
+                    // convert bool fields
+                    if ($key === 'id') {
+                        $mapping[$key] = ('true' === $mapping[$key]) ? true : false;
+                    }
+                }
+                $this->addPropertyMapping($class, $mapping);
+            }
+        }
+        if (isset($xmlRoot->path)) {
+            $mapping = array('fieldName' => (string) $xmlRoot->path->attributes()->name);
+            $this->addPathMapping($class, $mapping);
+        }
+        if (isset($xmlRoot->node)) {
+            $mapping = array('fieldName' => (string) $xmlRoot->node->attributes()->name);
+            $this->addNodeMapping($class, $mapping);
+        }
+        if (isset($xmlRoot->{'reference-many'})) {
+            foreach ($xmlRoot->{'reference-many'} as $reference) {
+                $this->addReferenceMapping($class, $reference, 'many');
+            }
+        }
+        if (isset($xmlRoot->{'reference-one'})) {
+            foreach ($xmlRoot->{'reference-one'} as $reference) {
+                $this->addReferenceMapping($class, $reference, 'one');
+            }
+        }
+    }
+
+    private function addPropertyMapping(ClassMetadata $class, $mapping)
+    {
+        $class->mapProperty($mapping);
+    }
+
+    private function addPathMapping(ClassMetadata $class, $mapping)
+    {
+        $class->mapPath($mapping);
+    }
+
+    private function addNodeMapping(ClassMetadata $class, $mapping)
+    {
+        $class->mapNode($mapping);
+    }
+
+    private function addReferenceMapping(ClassMetadata $class, $reference, $type)
+    {
+        $cascade = array_keys((array) $reference->cascade);
+        if (1 === count($cascade)) {
+            $cascade = current($cascade) ?: next($cascade);
+        }
+        $attributes = $reference->attributes();
+        $mapping = array(
+            'cascade'        => $cascade,
+            'type'           => $type,
+            'reference'      => true,
+            'targetDocument' => isset($attributes['target-document']) ? (string) $attributes['target-document'] : null,
+            'name'           => (string) $attributes['field'],
+        );
+        $this->addFieldMapping($class, $mapping);
+    }
+
+    protected function loadMappingFile($file)
+    {
+        $result = array();
+        $xmlElement = simplexml_load_file($file);
+
+        foreach (array('document', 'mapped-superclass') as $type) {
+            if (isset($xmlElement->$type)) {
+                foreach ($xmlElement->$type as $documentElement) {
+                    $documentName = (string) $documentElement['name'];
+                    $result[$documentName] = $documentElement;
+                }
+            }
+        }
+
+        return $result;
+    }
+}
---
+++ lib/Doctrine/ODM/PHPCR/Mapping/Driver/YamlDriver.php
@@ -0,0 +1,131 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the LGPL. For more information, see
+ * <http://www.doctrine-project.org>.
+ */
+
+namespace Doctrine\ODM\PHPCR\Mapping\Driver;
+
+use Doctrine\ODM\PHPCR\Mapping\ClassMetadata;
+
+/**
+ * The YamlDriver reads the mapping metadata from yaml schema files.
+ *
+ * @license     http://www.opensource.org/licenses/lgpl-license.php LGPL
+ * @link        www.doctrine-project.org
+ * @since       1.0
+ * @author      Jonathan H. Wage <jonwage@gmail.com>
+ * @author      Roman Borschel <roman@code-factory.org>
+ */
+class YamlDriver extends AbstractFileDriver
+{
+    /**
+     * The file extension of mapping documents.
+     *
+     * @var string
+     */
+    protected $fileExtension = '.dcm.yml';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function loadMetadataForClass($className, ClassMetadata $class)
+    {
+        $element = $this->getElement($className);
+        if (!$element) {
+            return;
+        }
+        $element['type'] = isset($element['type']) ? $element['type'] : 'document';
+
+        if ($element['type'] == 'document') {
+            if (isset($element['repositoryClass'])) {
+                $class->setCustomRepositoryClass($element['repositoryClass']);
+            }
+            if (!isset($element['alias'])) {
+                throw MappingException::aliasIsNotSpecified($className);
+            }
+            $class->setAlias($element['alias']);
+            $class->setNodeType(isset($element['nodeType']) ? $element['nodeType'] : 'nt:unstructured');
+        } elseif ($element['type'] === 'mappedSuperclass') {
+            $class->isMappedSuperclass = true;
+        }
+
+        if (isset($element['fields'])) {
+            foreach ($element['fields'] as $fieldName => $mapping) {
+                if (is_string($mapping)) {
+                    $type = $mapping;
+                    $mapping = array();
+                    $mapping['type'] = $type;
+                }
+                if (!isset($mapping['fieldName'])) {
+                    $mapping['fieldName'] = $fieldName;
+                }
+                $this->addPropertyMapping($class, $mapping);
+            }
+        }
+        if (isset($element['path'])) {
+            $mapping = array('fieldName' => $element['path']);
+            $this->addPathMapping($class, $mapping);
+        }
+        if (isset($element['node'])) {
+            $mapping = array('fieldName' => $element['node']);
+            $this->addNodeMapping($class, $mapping);
+        }
+        if (isset($element['referenceOne'])) {
+            foreach ($element['referenceOne'] as $fieldName => $reference) {
+                $this->addMappingFromReference($class, $fieldName, $reference, 'one');
+            }
+        }
+        if (isset($element['referenceMany'])) {
+            foreach ($element['referenceMany'] as $fieldName => $reference) {
+                $this->addMappingFromReference($class, $fieldName, $reference, 'many');
+            }
+        }
+    }
+
+    private function addPropertyMapping(ClassMetadata $class, $mapping)
+    {
+        $class->mapProperty($mapping);
+    }
+
+    private function addPathMapping(ClassMetadata $class, $mapping)
+    {
+        $class->mapPath($mapping);
+    }
+
+    private function addNodeMapping(ClassMetadata $class, $mapping)
+    {
+        $class->mapNode($mapping);
+    }
+
+    private function addMappingFromReference(ClassMetadata $class, $fieldName, $reference, $type)
+    {
+        $mapping = array(
+            'cascade'        => isset($reference['cascade']) ? $reference['cascade'] : null,
+            'type'           => $type,
+            'reference'      => true,
+            'targetDocument' => isset($reference['targetDocument']) ? $reference['targetDocument'] : null,
+            'fieldName'      => $fieldName,
+            'strategy'       => isset($reference['strategy']) ? (string) $reference['strategy'] : 'pushPull',
+        );
+        $this->addFieldMapping($class, $mapping);
+    }
+
+    protected function loadMappingFile($file)
+    {
+        return \Symfony\Component\Yaml\Yaml::load($file);
+    }
+}
