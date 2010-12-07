35191e23019803c10265e6d225df6bf0e2f9c619
Jordi Boggiano <j.boggiano@seld.be>
2010-12-07 16:58:23 +0100

    Added some new MetadataException methods

--- lib/Doctrine/ODM/PHPCR/Mapping/MappingException.php
+++ lib/Doctrine/ODM/PHPCR/Mapping/MappingException.php
@@ -28,9 +28,30 @@ class MappingException extends \Exception
         return new self('An error occurred in ' . $document, 0, $previousException);
     }
 
+    /**
+     * @param string $document The document's name
+     * @param string $fieldName The name of the field that was already declared
+     */
+    public static function duplicateFieldMapping($document, $fieldName) {
+        return new self('Property "'.$fieldName.'" in "'.$document.'" was already declared, but it must be declared only once');
+    }
+
+    /**
+     * @param string $document The document's name
+     * @param string $fieldName The name of the field that was already declared
+     */
+    public static function missingTypeDefinition($document, $fieldName) {
+        return new self('Property "'.$fieldName.'" in "'.$document.'" must have a type attribute defined');
+    }
+
     public static function fileMappingDriversRequireConfiguredDirectoryPath()
     {
         return new self('File mapping drivers must have a valid directory path, however the given path seems to be incorrect!');
+    }
+
+    public static function aliasIsNotSpecified($document)
+    {
+        return new self('Document '.$document.' must specify an alias');
     }
 
     public static function classNotMapped()
