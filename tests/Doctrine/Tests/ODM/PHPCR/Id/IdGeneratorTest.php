e77e3acf5146005526382fc60529fddd2b9da0be
Roland Schilter <r@rndstr.org>
2011-04-13 08:47:17 +0200

    added more unit tests to the id generator

---
+++ tests/Doctrine/Tests/ODM/PHPCR/Id/IdGeneratorTest.php
@@ -0,0 +1,35 @@
+<?php
+
+use Doctrine\ODM\PHPCR\Id\IdGenerator;
+use Doctrine\ODM\PHPCR\Mapping\ClassMetadata;
+
+class IdGeneratorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers Doctrine\ODM\PHPCR\Id\IdGenerator::create
+     */
+    public function testCreateGeneratorTypeAssigned()
+    {
+        $generator = IdGenerator::create(ClassMetadata::GENERATOR_TYPE_ASSIGNED);
+        $this->assertInstanceOf('Doctrine\ODM\PHPCR\Id\AssignedIdGenerator', $generator);
+    }
+
+    /**
+     * @covers Doctrine\ODM\PHPCR\Id\IdGenerator::create
+     */
+    public function testCreateGeneratorTypeRepository()
+    {
+        $generator = IdGenerator::create(ClassMetadata::GENERATOR_TYPE_REPOSITORY);
+        $this->assertInstanceOf('Doctrine\ODM\PHPCR\Id\RepositoryIdGenerator', $generator);
+    }
+
+    /**
+     * @expectedException \Exception
+     * @covers Doctrine\ODM\PHPCR\Id\IdGenerator::create
+     */
+    public function testCreateException()
+    {
+        $generator = IdGenerator::create(null);
+    }
+}
+
