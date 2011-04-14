5b2d37b68b29cdcbca0a27590ecd0cde4c4ba042
dbu <david.buchmann@liip.ch>
2011-04-14 18:48:43 +0200

    remove trailing whitespaces

--- lib/Doctrine/ODM/PHPCR/Tools/Console/Command/RegisterNodeTypesCommand.php
+++ lib/Doctrine/ODM/PHPCR/Tools/Console/Command/RegisterNodeTypesCommand.php
@@ -38,11 +38,11 @@ This command allows to register node types in the repository that are defined
 in a CND (Compact Node Definition) file.
 
 Custom node types can be used to define the structure of content repository
-nodes, like allowed properties and child nodes together with the namespaces  
+nodes, like allowed properties and child nodes together with the namespaces
 and their prefix used for the names of node types and properties.
 
-If you use --allow-update existing node type definitions will be overwritten 
-in the repository. 
+If you use --allow-update existing node type definitions will be overwritten
+in the repository.
 EOT
         );
     }
@@ -78,7 +78,7 @@ EOT
                 $output->write('<info>--allow-update</info> option.'.PHP_EOL);
             }
             throw $e;
-        }   
+        }
         $output->write(PHP_EOL.sprintf('Sucessfully registered node types from "<info>%s</info>"', $cnd_file) . PHP_EOL);
     }
 }
