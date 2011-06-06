6e13a7371ab77f8ccab0f03faf234355a86be621
Michele Orselli <mo@ideato.it>
2011-06-06 14:10:10 +0200

    removed doctrine 2.1 code

--- lib/Doctrine/ODM/PHPCR/Configuration.php
+++ lib/Doctrine/ODM/PHPCR/Configuration.php
@@ -154,8 +154,6 @@ class Configuration
     public function newDefaultAnnotationDriver($paths = array())
     {
         $reader = new \Doctrine\Common\Annotations\AnnotationReader();
-        $reader->setDefaultAnnotationNamespace('Doctrine\ODM\PHPCR\Mapping\Annotations\\');
-        $reader->setAutoloadAnnotations(false);
         
         return new \Doctrine\ODM\PHPCR\Mapping\Driver\AnnotationDriver($reader, (array) $paths);
     }
