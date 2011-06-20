3d471a7e9130bb7f25e405fb060d17332f3ab1b0
Lukas Kahwe Smith <smith@pooteeweet.org>
2011-06-20 16:49:53 +0200

    warning fix

--- lib/Doctrine/ODM/PHPCR/DocumentRepository.php
+++ lib/Doctrine/ODM/PHPCR/DocumentRepository.php
@@ -109,6 +109,7 @@ class DocumentRepository implements ObjectRepository
         $uow = $this->dm->getUnitOfWork();
 
         $nodes = $this->dm->getPhpcrSession()->getNodes($ids);
+        $documents = array();
         foreach ($nodes as $node) {
             $documents[$node->getPath()] = $this->createDocument($uow, $this->documentName, $node);
         }
