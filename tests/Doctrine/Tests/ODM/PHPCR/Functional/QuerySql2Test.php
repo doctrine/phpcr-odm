e9b6c53ec14b0c93c5bdc663d5dbd08a48e22952
dbu <david.buchmann@liip.ch>
2011-10-24 11:25:27 +0200

    adding tests for searching, readme example on searching and updating to latest jackalope

---
+++ tests/Doctrine/Tests/ODM/PHPCR/Functional/QuerySql2Test.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Doctrine\Tests\ODM\PHPCR\Functional;
+
+use Doctrine\ODM\PHPCR\Mapping\Annotations as PHPCRODM;
+
+/**
+ * @group functional
+ */
+class QuerySql2Test extends \Doctrine\Tests\ODM\PHPCR\PHPCRFunctionalTestCase
+{
+    /**
+     * @var DocumentManager
+     */
+    private $dm;
+
+    private $type;
+
+    private $node;
+
+    public function queryStatements()
+    {
+        return array(
+            array('SELECT username FROM [nt:unstructured] WHERE ISCHILDNODE("/functional")', 5),
+            array('SELECT username FROM [nt:unstructured] WHERE ISCHILDNODE("/functional") ORDER BY username', 5),
+            array('SELECT username FROM [nt:unstructured] WHERE ISCHILDNODE("/functional") AND username="dbu"', 1),
+            array('SELECT username FROM [nt:unstructured] WHERE ISCHILDNODE("/functional") AND username="notexisting"', 0),
+            array('invalidstatement', -1),
+            // TODO: try a join
+        );
+    }
+
+    public function setUp()
+    {
+        $this->type = 'Doctrine\Tests\ODM\PHPCR\Functional\QuerySql2TestObj';
+        $this->dm = $this->createDocumentManager();
+        $this->node = $this->resetFunctionalNode($this->dm);
+
+        $versionNode = $this->node->addNode('node1');
+        $versionNode->setProperty('username', 'dbu');
+        $versionNode->setProperty('numbers', array(3, 1, 2));
+        $versionNode->setProperty('phpcr:class', $this->type);
+
+        $versionNode = $this->node->addNode('node2');
+        $versionNode->setProperty('username', 'johannes');
+        $versionNode->setProperty('numbers', array(3, 1, 2));
+        $versionNode->setProperty('phpcr:class', $this->type);
+
+        $versionNode = $this->node->addNode('node3');
+        $versionNode->setProperty('username', 'lsmith');
+        $versionNode->setProperty('numbers', array(3, 1, 2));
+        $versionNode->setProperty('phpcr:class', $this->type);
+
+        $versionNode = $this->node->addNode('node4');
+        $versionNode->setProperty('username', 'uwe');
+        $versionNode->setProperty('numbers', array(3, 1, 2));
+        $versionNode->setProperty('phpcr:class', $this->type);
+
+        $versionNode = $this->node->addNode('node5');
+        $versionNode->setProperty('numbers', array(3, 1, 2));
+        $versionNode->setProperty('phpcr:class', $this->type);
+
+        $this->dm->getPhpcrSession()->save();
+        $this->dm = $this->createDocumentManager();
+    }
+
+    /**
+     * @dataProvider queryStatements
+     */
+    public function testQuery($statement, $rowCount)
+    {
+        if ($rowCount == -1) {
+            // magic to tell this is an invalid query
+            $this->setExpectedException('PHPCR\Query\InvalidQueryException');
+        }
+        $query = $this->dm->createQuery($statement, \PHPCR\Query\QueryInterface::JCR_SQL2);
+        $this->assertInstanceOf('PHPCR\Query\QueryInterface', $query);
+
+        $result = $this->dm->getDocumentsByQuery($query, $this->type);
+        $this->assertEquals($rowCount, count($result));
+    }
+
+    public function testQueryLimit()
+    {
+        $query = $this->dm->createQuery('SELECT * FROM [nt:unstructured] WHERE ISCHILDNODE("/functional") ORDER BY username',
+                                        \PHPCR\Query\QueryInterface::JCR_SQL2);
+        $this->assertInstanceOf('PHPCR\Query\QueryInterface', $query);
+        $query->setLimit(2);
+        $result = $this->dm->getDocumentsByQuery($query, $this->type);
+        $this->assertEquals(2, count($result));
+        $ids = array();
+        $vals = array();
+        $nums = array();
+        foreach ($result as $obj) {
+            $this->assertInstanceOf('Doctrine\Tests\ODM\PHPCR\Functional\QuerySql2TestObj', $obj);
+            $ids[] = $obj->id;
+            $vals[] = $obj->username;
+            $this->assertInstanceOf('Doctrine\Common\Collections\ArrayCollection', $obj->numbers);
+            $nums[] = $obj->numbers->toArray();
+        }
+        $this->assertEquals(array('/functional/node5', '/functional/node1'), $ids);
+        $this->assertEquals(array(null, 'dbu'), $vals);
+        $this->assertEquals(array(array(3,1,2),array(3,1,2)), $nums);
+    }
+}
+
+/**
+ * @PHPCRODM\Document(alias="versionTestObj")
+ */
+class QuerySql2TestObj
+{
+    /** @PHPCRODM\Id */
+    public $id;
+    /** @PHPCRODM\Node */
+    public $node;
+    /** @PHPCRODM\String(name="username") */
+    public $username;
+    /** @PHPCRODM\Int(name="numbers", multivalue=true) */
+    public $numbers;
+}
