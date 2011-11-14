7dcd324ce0ba17c7728b0b1b5bee58b2fd2128ad
Uwe Jäger <uwej711@googlemail.com>
2011-11-14 23:19:34 +0100

    added test for non-referenceable documents - still failing

--- tests/Doctrine/Tests/Models/References/RefTestPrivateObj.php
+++ tests/Doctrine/Tests/Models/References/RefTestPrivateObj.php
@@ -5,7 +5,7 @@ namespace Doctrine\Tests\Models\References;
 use Doctrine\ODM\PHPCR\Mapping\Annotations as PHPCRODM;
 
 /**
- * @PHPCRODM\Document(alias="RefTestObj")
+ * @PHPCRODM\Document(alias="RefTestPrivateObj")
  */
 class RefTestPrivateObj
 {
