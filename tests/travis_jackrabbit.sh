3f6e2c113e260918a9f43144ff3da561851638a7
Lukas Kahwe Smith <smith@pooteeweet.org>
2011-11-15 17:41:19 +0100

    further travis tweaks ..

--- tests/travis_jackrabbit.sh
+++ tests/travis_jackrabbit.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
 git submodule update --init --recursive
-wget http://s3-eu-west-1.amazonaws.com/patched-jackrabbit/jackrabbit-standalone-2.3.1-jackalope-SNAPSHOT.jar
-java -jar jackrabbit-standalone-2.3.1-jackalope-SNAPSHOT.jar&
 
-echo "Waiting until Jackrabbit is ready on port 8080"
-while [[ -z `curl -s 'http://localhost:8080' ` ]]
-do
-        echo -n "."
-        sleep 2s
-done
+./lib/vendor/jackalope/tests/travis_jackrabbit.sh
 
-echo "Jackrabbit is up"
+cp cli-config.php.dist cli-config.php
+./bin/phpcr odm:phpcr:register-system-node-types
