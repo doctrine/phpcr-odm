715e7ec6f957fbb6c597dbb0810e31732c421f13
Daniel Barsotti <daniel.barsotti@liip.ch>
2011-11-22 13:18:35 +0100

    Implemented the skeleton of ChildTranslationStrategy

--- lib/Doctrine/ODM/PHPCR/Translation/TranslationStrategy/ChildTranslationStrategy.php
+++ lib/Doctrine/ODM/PHPCR/Translation/TranslationStrategy/ChildTranslationStrategy.php
@@ -2,15 +2,34 @@
 
 namespace Doctrine\ODM\PHPCR\Translation\TranslationStrategy;
 
-class ChildTranslationStrategy //implements TranslationStrategyInterface
+class ChildTranslationStrategy implements TranslationStrategyInterface
 {
-    function saveTranslations($document, $lang)
+    public function saveTranslation($document, NodeInterface $node, ClassMetadata $metadata, $lang)
     {
-
+        throw new \Exception('Not implemented');
     }
 
-    function loadTranslations($document, $lang)
+    /**
+     * {@inheritdoc}
+     */
+    public function loadTranslation($document, NodeInterface $node, ClassMetadata $metadata, $lang)
     {
+        throw new \Exception('Not implemented');
+    }
 
+    /**
+     * {@inheritdoc}
+     */
+    public function removeTranslation($document, NodeInterface $node, ClassMetadata $metadata, $lang)
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function removeAllTranslations($document, NodeInterface $node, ClassMetadata $metadata)
+    {
+        throw new \Exception('Not implemented');
     }
 }
