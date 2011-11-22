d84756eee1102ce59eab39a0c007db33af86e907
Daniel Barsotti <daniel.barsotti@liip.ch>
2011-11-22 09:51:53 +0100

    Added PHPDoc to the TranslationStrategyInterface

--- lib/Doctrine/ODM/PHPCR/Translation/TranslationStrategy/TranslationStrategyInterface.php
+++ lib/Doctrine/ODM/PHPCR/Translation/TranslationStrategy/TranslationStrategyInterface.php
@@ -7,11 +7,42 @@ use Doctrine\ODM\PHPCR\Mapping\ClassMetadata,
 
 interface TranslationStrategyInterface
 {
+    /**
+     * Save the translatable fields of a node
+     * @abstract
+     * @param $document The document containing the data
+     * @param \PHPCR\NodeInterface $node The physical node in the content repository
+     * @param \Doctrine\ODM\PHPCR\Mapping\ClassMetadata $metadata The Doctrine metadata of the document
+     * @param $lang The language to persist the translations to
+     */
     public function saveTranslations($document, NodeInterface $node, ClassMetadata $metadata, $lang);
 
+    /**
+     * Load the translatable fields of a node
+     * @abstract
+     * @param $document The document in which to load the data
+     * @param \PHPCR\NodeInterface $node The physical node in the content repository
+     * @param \Doctrine\ODM\PHPCR\Mapping\ClassMetadata $metadata The Doctrine metadata of the document
+     * @param $lang The language to load the translations from
+     */
     public function loadTranslations($document, NodeInterface $node, ClassMetadata $metadata, $lang);
 
+    /**
+     * Removes all the translated fields of a node
+     * @abstract
+     * @param $document The document from which the translations must be removed
+     * @param \PHPCR\NodeInterface $node The physical node in the content repository
+     * @param \Doctrine\ODM\PHPCR\Mapping\ClassMetadata $metadata The Doctrine metadata of the document
+     */
     public function removeAllTranslations($document, NodeInterface $node, ClassMetadata $metadata);
 
+    /**
+     * Remove the translated fields of a node in a given language
+     * @abstract
+     * @param $document The document from which the translations must be removed
+     * @param \PHPCR\NodeInterface $node The physical node in the content repository
+     * @param \Doctrine\ODM\PHPCR\Mapping\ClassMetadata $metadata The Doctrine metadata of the document
+     * @param $lang The language to remove
+     */
     public function removeTranslation($document, NodeInterface $node, ClassMetadata $metadata, $lang);
 }
