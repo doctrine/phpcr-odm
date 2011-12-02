48c8d0d2490b45ffcf26dbbed8d8e2af6aec1961
Daniel Barsotti <daniel.barsotti@liip.ch>
2011-12-02 10:14:51 +0100

    Extracted abstract class AbstractTranslationStrategy

--- lib/Doctrine/ODM/PHPCR/Translation/TranslationStrategy/AttributeTranslationStrategy.php
+++ lib/Doctrine/ODM/PHPCR/Translation/TranslationStrategy/AttributeTranslationStrategy.php
@@ -14,20 +14,8 @@ use Doctrine\ODM\PHPCR\Translation\Translation;
  * @since       1.0
  * @author      Daniel Barsotti <daniel.barsotti@liip.ch>
  */
-class AttributeTranslationStrategy implements TranslationStrategyInterface
+class AttributeTranslationStrategy extends AbstractTranslationStrategy
 {
-    /*** @var string */
-    protected $prefix = Translation::LOCALE_NAMESPACE;
-
-    /**
-     * Set the prefix to use to determine the name of the property where translations are stored
-     * @param $prefix
-     */
-    public function setPrefix($prefix)
-    {
-        $this->prefix = $prefix;
-    }
-
     /**
      * {@inheritdoc}
      */
@@ -95,16 +83,5 @@ class AttributeTranslationStrategy implements TranslationStrategyInterface
             }
         }
         return $locales;
-    }
-
-    /**
-     * Get the name of the property where to store the translations of a given property in a given language
-     * @param $locale The language to store
-     * @param $fieldName The name of the field to translate
-     * @return string The name of the property where to store the translation
-     */
-    protected function getTranslatedPropertyName($locale, $fieldName)
-    {
-        return sprintf('%s-%s-%s', $this->prefix, $locale, $fieldName);
     }
 }
