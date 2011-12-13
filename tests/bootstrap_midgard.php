c94139f87ebe848fedf57c49609b8a5b344a5271
Henri Bergius <henri.bergius@iki.fi>
2011-12-13 15:21:06 +0200

    Disable GC for tests

--- tests/bootstrap_midgard.php
+++ tests/bootstrap_midgard.php
@@ -1,4 +1,11 @@
 <?php
+if (gc_enabled()) {
+    echo "Disabling Zend Garbage Collection to prevent segfaults, see:\n";
+    echo "  https://bugs.php.net/bug.php?id=51091\n";
+    echo "  https://github.com/midgardproject/midgard-php5/issues/50\n";
+    gc_disable(); 
+}
+
 require_once __DIR__ . '/bootstrap.php';
 
 use Doctrine\Common\ClassLoader;
