82eb287eae4c75d5122a58900094c74016d61976
Henri Bergius <henri.bergius@iki.fi>
2011-12-15 11:26:28 +0200

    Copy schemas with sudo

--- tests/travis_midgard.sh
+++ tests/travis_midgard.sh
@@ -6,5 +6,5 @@ git submodule update --init --recursive
 ./lib/vendor/Midgard/PHPCR/tests/travis_midgard.sh
 
 # Copy PHPCR schemas to Midgard's global schema dir
-cp -r ./lib/vendor/Midgard/PHPCR/data/share/* /usr/share/midgard2
-cp ./data/share/odm_schemas.xml /usr/share/midgard2/schema
+sudo cp -r ./lib/vendor/Midgard/PHPCR/data/share/* /usr/share/midgard2
+sudo cp ./data/share/odm_schemas.xml /usr/share/midgard2/schema
