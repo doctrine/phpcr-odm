b24c83d2f3772cac062ff2e6d48aceefee6e4031
lsmith77 <smith@pooteeweet.org>
2011-12-22 17:22:45 +0100

    initial support for Doctrine DBAL

--- tests/bootstrap_doctrine_dbal.php
+++ tests/bootstrap_doctrine_dbal.php
@@ -1,8 +1,13 @@
 <?php
 
-require_once __DIR__ . '/bootstrap.php';
+require_once __DIR__ . '/../lib/vendor/doctrine-common/lib/Doctrine/Common/ClassLoader.php';
 
 use Doctrine\Common\ClassLoader;
 
-$classLoader = new ClassLoader('Doctrine\DBAL', __DIR__ . '/../../doctrine-dbal/lib');
+$classLoader = new ClassLoader('Doctrine\DBAL', __DIR__ . '/../lib/vendor/jackalope/lib/vendor/doctrine-dbal/lib');
 $classLoader->register();
+
+$classLoader = new ClassLoader('Jackalope', __DIR__ . '/../lib/vendor/jackalope/src');
+$classLoader->register();
+
+require_once __DIR__ . '/bootstrap.php';
--- tests/bootstrap_jackrabbit.php
+++ tests/bootstrap_jackrabbit.php
@@ -1,3 +1,6 @@
 <?php
 
 require_once __DIR__ . '/bootstrap.php';
+
+$classLoader = new ClassLoader('Jackalope', __DIR__ . '/../lib/vendor/jackalope/src');
+$classLoader->register();
