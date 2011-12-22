64405c04657b9421167b129ec2dfa16d9a29bb5a
lsmith77 <smith@pooteeweet.org>
2011-12-22 17:35:59 +0100

    fix bootstrap of jackrabbit

--- tests/bootstrap_jackrabbit.php
+++ tests/bootstrap_jackrabbit.php
@@ -1,6 +1,10 @@
 <?php
 
-require_once __DIR__ . '/bootstrap.php';
+require_once __DIR__ . '/../lib/vendor/doctrine-common/lib/Doctrine/Common/ClassLoader.php';
+
+use Doctrine\Common\ClassLoader;
 
 $classLoader = new ClassLoader('Jackalope', __DIR__ . '/../lib/vendor/jackalope/src');
 $classLoader->register();
+
+require_once __DIR__ . '/bootstrap.php';
