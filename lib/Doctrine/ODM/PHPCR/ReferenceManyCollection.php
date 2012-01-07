57fbd21017226f776cc93339bcc0febf800c33ff
Stark <johannes.stark@gmx.de>
2012-01-07 12:09:02 +0100

    Let ReferenceManyCollection inherit from MultivaluePropertyCollection as they have the same functionality

--- lib/Doctrine/ODM/PHPCR/ReferenceManyCollection.php
+++ lib/Doctrine/ODM/PHPCR/ReferenceManyCollection.php
@@ -9,21 +9,7 @@ use Doctrine\Common\Collections\Collection;
  *
  * This class stores all documents or their proxies referenced by a reference many property 
  */
-class ReferenceManyCollection extends PersistentCollection
+class ReferenceManyCollection extends MultivaluePropertyCollection
 {
-    public function __construct(Collection $coll, $isDirty = false)
-    {
-        $this->coll = $coll;
-        $this->isDirty = $isDirty;
-        $this->initialized = true;
-    }
 
-    /**
-     * Just there to fulfill the interface requirements of PersistentCollection
-     */
-    public function initialize()
-    {
-        $this->initialized = true;
-    }
 }
-
