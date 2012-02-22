8045a98088456f3ef7f022722cec118a86f15990
Christian Stocker <me@chregu.tv>
2012-02-22 09:30:43 +0100

    Get Session from DM

--- lib/Doctrine/ODM/PHPCR/ReferenceManyCollection.php
+++ lib/Doctrine/ODM/PHPCR/ReferenceManyCollection.php
@@ -13,7 +13,6 @@ use Doctrine\Common\Collections\ArrayCollection;
 class ReferenceManyCollection extends MultivaluePropertyCollection
 {
 
-    private $session;
     private $referencedDocUUIDs;
     private $targetDocument = null;
     
@@ -21,14 +20,12 @@ class ReferenceManyCollection extends MultivaluePropertyCollection
      * Creates a new persistent collection.
      *
      * @param DocumentManager $dm The DocumentManager the collection will be associated with.
-     * @param \PHPCR\SessionInterface $session The current session
      * @param array $referencedDocUUIDs An array of referenced UUIDs
      * @param string $targetDocument the objectname of the target documents
      */
-    public function __construct(DocumentManager $dm, \PHPCR\SessionInterface $session, array $referencedDocUUIDs, $targetDocument)
+    public function __construct(DocumentManager $dm, array $referencedDocUUIDs, $targetDocument)
     {
         $this->dm = $dm;
-        $this->session = $session;
         $this->referencedDocUUIDs = $referencedDocUUIDs;
         $this->targetDocument = $targetDocument;
     }
@@ -43,7 +40,7 @@ class ReferenceManyCollection extends MultivaluePropertyCollection
             $this->initialized = true;
 
             $referencedDocs = array();
-            $referencedNodes = $this->session->getNodesByIdentifier($this->referencedDocUUIDs);
+            $referencedNodes = $this->dm->getPhpcrSession()->getNodesByIdentifier($this->referencedDocUUIDs);
             $uow = $this->dm->getUnitOfWork();
             foreach ($referencedNodes as $referencedNode) {
                 $referencedClass = $this->targetDocument ? $this->dm->getMetadataFactory()->getMetadataFor(ltrim($this->targetDocument, '\\'))->name : null;
