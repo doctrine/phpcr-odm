1e9594952d5125fe4f0c0702f85478605d0b55fa
lsmith77 <smith@pooteeweet.org>
2012-03-18 16:19:56 -0400

    minor tweak

--- bootstrap.php
+++ bootstrap.php
@@ -2,32 +2,15 @@
 
 $vendorDir = __DIR__.'/vendor';
 
-spl_autoload_register(function($class)
-{
-    if (0 === strpos($class, 'Doctrine\ODM\PHPCR')) {
-        $path = __DIR__.'/lib/'.implode('/', explode('\\', $class)).'.php';
-        if (!stream_resolve_include_path($path)) {
-            return false;
-        }
-        require_once $path;
-        return true;
-    }
-    if (0 === strpos($class, 'Doctrine\Tests')) {
-        $path = __DIR__.'/tests/'.implode('/', explode('\\', $class)).'.php';
-        if (!stream_resolve_include_path($path)) {
-            return false;
-        }
-        require_once $path;
-        return true;
-    }
-});
-
 $file = $vendorDir.'/.composer/autoload.php';
 if (file_exists($file)) {
     $autoload = require_once $file;
 } else {
     throw new RuntimeException('Install dependencies to run test suite.');
 }
+
+$autoload->add('Doctrine\ODM\PHPCR', __DIR__.'/lib');
+$autoload->add('Doctrine\Tests', __DIR__.'/tests');
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
