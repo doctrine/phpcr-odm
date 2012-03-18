b1162a7b9e3139d3a4004bf80b563cdd38ec1a3d
lsmith77 <smith@pooteeweet.org>
2012-03-18 16:24:56 -0400

    updated to match latest jackalope-jackrabbit

--- tests/travis_jackrabbit.sh
+++ tests/travis_jackrabbit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./vendor/jackalope/jackalope-jackrabbit/tests/travis.sh
+./vendor/jackalope/jackalope-jackrabbit/tests/jackrabbit.sh
 
 cp cli-config.jackrabbit.php.dist cli-config.php
 ./bin/phpcr doctrine:phpcr:register-system-node-types
