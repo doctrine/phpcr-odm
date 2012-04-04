e57e82a28484efd5133750c06dd1739c30632ec6
piotras <piotrek.pokora@gmail.com>
2012-04-04 12:32:48 +0200

    sudo before downloading file

--- tests/travis_midgard_install.sh
+++ tests/travis_midgard_install.sh
@@ -6,8 +6,8 @@ chmod +x ./travis_midgard.sh
 ./travis_midgard.sh
 
 # Copy PHPCR schemas to Midgard's global schema dir
-wget --directory-prefix=/usr/share/midgard2/schema https://github.com/midgardproject/phpcr-midgard2/raw/master/data/share/schema/midgard_namespace_registry.xml
-wget --directory-prefix=/usr/share/midgard2/schema https://github.com/midgardproject/phpcr-midgard2/raw/master/data/share/schema/midgard_tree_node.xml
+sudo wget --directory-prefix=/usr/share/midgard2/schema https://github.com/midgardproject/phpcr-midgard2/raw/master/data/share/schema/midgard_namespace_registry.xml
+sudo wget --directory-prefix=/usr/share/midgard2/schema https://github.com/midgardproject/phpcr-midgard2/raw/master/data/share/schema/midgard_tree_node.xml
 
 # Copy odm schema
 sudo cp ./data/share/odm_schemas.xml /usr/share/midgard2/schema
