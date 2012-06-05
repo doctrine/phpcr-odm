26360b7000b7f416044d5b9c68d219e81666de67
David Buchmann <david.buchmann@liip.ch>
2012-06-05 14:42:46 +0200

    adjusting midgard install to work with composer --dev

--- tests/travis_midgard_mysql.sh
+++ tests/travis_midgard_mysql.sh
@@ -6,9 +6,6 @@ sudo apt-get install -y libgda-4.0-mysql
 # Create the database
 mysql -e 'create database midgard2_test;'
 
-# Install Midgard2
-./tests/travis_midgard_install.sh
-
 # Set up CLI and install namespaces
 cp cli-config.midgard_mysql.php.dist cli-config.php
 php bin/phpcr doctrine:phpcr:register-system-node-types
--- tests/travis_midgard_sqlite.sh
+++ tests/travis_midgard_sqlite.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Install Midgard2
-./tests/travis_midgard_install.sh
-
 # Set up CLI and install namespaces
 cp cli-config.midgard_sqlite.php.dist cli-config.php
 php bin/phpcr doctrine:phpcr:register-system-node-types
