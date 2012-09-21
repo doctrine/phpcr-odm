fc6e0c5bff77ef4c59eb51019083f0d21440d9c5
Beau Simensen <beau@dflydev.com>
2012-09-21 06:40:45 -0700

    Added test for adding multiple references
    Ensure that adding multiple references is persisted as expected.

--- tests/Doctrine/Tests/ODM/PHPCR/Functional/ReferenceTest.php
+++ tests/Doctrine/Tests/ODM/PHPCR/Functional/ReferenceTest.php
@@ -500,6 +500,42 @@ class ReferenceTest extends \Doctrine\Tests\ODM\PHPCR\PHPCRFunctionalTestCase
         $this->assertEquals(0, count($refManyTestObj->references));
     }
 
+    public function testAddMultipleReferences()
+    {
+        $refManyTestObj = new RefManyTestObj();
+        $refRefTestObjA = new RefRefTestObj();
+        $refRefTestObjB = new RefRefTestObj();
+
+        $refManyTestObj->id = "/functional/refTestObj";
+        $refRefTestObjA->id = "/functional/refRefTestObjA";
+        $refRefTestObjA->name = "referencedA";
+        $refRefTestObjB->id = "/functional/refRefTestObjB";
+        $refRefTestObjB->name = "referencedB";
+
+        $this->assertEquals(0, count($refManyTestObj->references));
+
+        $this->dm->persist($refManyTestObj);
+        $this->dm->flush();
+        $this->dm->clear();
+
+        $refManyTestObj = $this->dm->find(null, "/functional/refTestObj");
+        $this->assertEquals(0, count($refManyTestObj->references));
+        $refManyTestObj->references[] = $refRefTestObjA;
+        $this->assertEquals(1, count($refManyTestObj->references));
+        $this->dm->flush();
+        $this->dm->clear();
+
+        $refManyTestObj = $this->dm->find(null, "/functional/refTestObj");
+        $this->assertEquals(1, count($refManyTestObj->references));
+        $refManyTestObj->references[] = $refRefTestObjB;
+        $this->assertEquals(2, count($refManyTestObj->references));
+        $this->dm->flush();
+        $this->dm->clear();
+
+        $refManyTestObj = $this->dm->find(null, "/functional/refTestObj");
+        $this->assertEquals(2, count($refManyTestObj->references));
+    }
+
     public function testRemoveReferrer()
     {
         $refTestObj = new RefTestObj();
