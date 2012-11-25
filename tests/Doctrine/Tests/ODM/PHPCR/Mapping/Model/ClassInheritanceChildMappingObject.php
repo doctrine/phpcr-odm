2115681c358373b1b754f03b3d9ad767f037f4ff
dantleech <daniel@ylyad.fr>
2012-11-25 17:05:41 +0100

    Fixed typo

--- tests/Doctrine/Tests/ODM/PHPCR/Mapping/Model/ClassInheritanceChildMappingObject.php
+++ tests/Doctrine/Tests/ODM/PHPCR/Mapping/Model/ClassInheritanceChildMappingObject.php
@@ -5,7 +5,7 @@ namespace Doctrine\Tests\ODM\PHPCR\Mapping\Model;
 use Doctrine\ODM\PHPCR\Mapping\Annotations as PHPCRODM;
 
 /**
- * A class that represents a parent class for the purposes
+ * A class that represents a child class for the purposes
  * of testing class property inheritance
  * 
  * @PHPCRODM\Document()
