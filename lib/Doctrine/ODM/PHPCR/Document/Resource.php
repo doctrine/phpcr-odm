801823061e9f1f3d0618b508aaf3a2e8932c54cf
Sven Cludius <sven.cludius@burda.com>
2012-12-12 13:03:09 +0100

    Add nodename to resource

--- lib/Doctrine/ODM/PHPCR/Document/Resource.php
+++ lib/Doctrine/ODM/PHPCR/Document/Resource.php
@@ -34,6 +34,9 @@ class Resource
 
     /** @PHPCRODM\Node */
     protected $node;
+    
+    /** @PHPCRODM\Nodename */
+    protected $nodename;
 
     /** @PHPCRODM\ParentDocument */
     protected $parent;
@@ -52,7 +55,27 @@ class Resource
 
     /** @PHPCRODM\String(name="jcr:lastModifiedBy") */
     protected $lastModifiedBy;
+    
+    /**
+     * The node name of the file.
+     *
+     * @return string
+     */
+    public function getNodename()
+    {
+        return $this->nodename;
+    }
 
+    /**
+     * Set the node name of the file. (only mutable on new document before the persist)
+     *
+     * @param string $name the name of the file
+     */
+    public function setNodename($name)
+    {
+        $this->nodename = $name;
+    }
+    
     /**
      * The parent File document of this Resource document.
      *
@@ -202,6 +225,6 @@ class Resource
      */
     public function __toString()
     {
-        return $this->getMime();
+        return $this->nodename;
     }
 }
