bbdbe93e632f8c809c6d38a1da9019a9a2f061e0
dantleech <dan.t.leech@gmail.com>
2012-12-28 22:09:38 +0000

    Refactoring query
    @at: Max results etc.

---
+++ lib/Doctrine/ODM/PHPCR/Query/QueryException.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Doctrine\ODM\PHPCR\Query;
+
+/**
+ * Mapping exception class
+ *
+ * @author      Daniel Leech <daniel@dantleech.com>
+ */
+class QueryException extends \Exception
+{
+    public static function hydrationModeNotKnown($hydrationMode)
+    {
+        return new self(sprintf(
+            'Hydration mode "%s" not recognized, must be the value of one of '.
+            'the Doctrine\ODM\PHPCR\Query::HYDRATE_ constants', 
+            $hydrationMode
+        ));
+    }
+
+    public static function nonUniqueResult()
+    {
+        return new self(sprintf('Expected unique unique result'));
+    }
+
+    public static function noResult()
+    {
+        return new self(sprintf('Expected result from query, didn\'t get one.'));
+    }
+}
