f050501e086aacf3f54466a242590c1d2a5965bb
dantleech <dan.t.leech@gmail.com>
2013-08-16 19:48:47 +0100

    Propvisional where

--- lib/Doctrine/ODM/PHPCR/Query/QueryBuilder/Where.php
+++ lib/Doctrine/ODM/PHPCR/Query/QueryBuilder/Where.php
@@ -7,7 +7,48 @@ class Where extends AbstractNode
     public function getCardinalityMap()
     {
         return array(
-            'Constraint' => array(1, 1),
+            'ConstraintInterface' => array(1, 1),
         );
+    }
+
+    public function andX()
+    {
+        return $this->addChild(new ConstraintAndx($this, $constraint1, $constraint2));
+    }
+
+    public function orX()
+    {
+        return $this->addChild(new ConstraintOrx($this, $constraint1, $constraint2));
+    }
+
+    public function propertyExists($propertyName, $selectorName)
+    {
+        return $this->addChild(new ConstraintPropertyExists($this, $propertyName, $selectorName));
+    }
+
+    public function fullTextSearch($propertyName, $fullTextSearchExpression, $selectorName)
+    {
+        return $this->addChild(new ConstraintFullTextSearch($this, $propertyName, $fullTextSearchExpression, $selectorName));
+    }
+
+
+    public function sameDocument($path, $selectorName)
+    {
+        return $this->addChild(new ConstraintSameDocument($this, $path, $selectorName));
+    }
+
+    public function descendantDocument($ancestorPath, $selectorName)
+    {
+        return $this->addChild(new ConstraintDescendantDocument($this, $ancestorPath, $selectorName));
+    }
+
+    public function childDocument($parentPath, $selectorName)
+    {
+        return $this->addChild(new ConstraintChildDocument($this, $parentPath, $selectorName));
+    }
+
+    public function not()
+    {
+        return $this->addChild(new ConstraintNot($this));
     }
 }
