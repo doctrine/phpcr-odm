a34946ad904b80e2d08db68236667712264563ec
David Buchmann <david.buchmann@liip.ch>
2013-10-29 17:09:25 +0100

    add some more tests on proxy behaviour

--- tests/Doctrine/Tests/ODM/PHPCR/Functional/ProxyTest.php
+++ tests/Doctrine/Tests/ODM/PHPCR/Functional/ProxyTest.php
@@ -38,4 +38,44 @@ class ProxyTest extends \Doctrine\Tests\ODM\PHPCR\PHPCRFunctionalTestCase
         $this->assertTrue(isset($user->name), "User is not set on demand");
         $this->assertEquals('Dominik', $user->name, "User is not loaded on demand");
     }
+
+    /**
+     * @depends testProxyProperty
+     */
+    public function testProxyUniqueness()
+    {
+        $user = new CmsUser();
+        $user->name = 'Dominik';
+        $user->username = 'domnikl';
+        $user->status = 'developer';
+
+        $this->dm->persist($user);
+        $this->dm->flush();
+        $this->dm->clear();
+
+        $user = $this->dm->getReference(get_class($user), $user->id);
+        $this->assertEquals('Dominik', $user->name, "User is not loaded on demand");
+
+        $this->assertSame($this->dm->getReference(get_class($user), $user->id), $user, 'Getting the proxy twice results in a copy');
+    }
+
+    public function testProxyImplicit()
+    {
+        $user = new CmsUser();
+        $user->name = 'Dominik';
+        $user->username = 'domnikl';
+        $user->status = 'developer';
+        $assistant = new CmsUser();
+        $assistant->username = 'bimbo';
+        $user->child = $assistant;
+
+        $this->dm->persist($user);
+        $this->dm->flush();
+        $this->dm->clear();
+
+        $user = $this->dm->find(null, $user->id);
+        $assistant = $this->dm->find(null, $user->id . '/assistant');
+
+        $this->assertSame($assistant, $user->child);
+    }
 }
