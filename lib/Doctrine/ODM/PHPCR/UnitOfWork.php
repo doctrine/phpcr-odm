23684b712c7ea9bfc56c63c68f03af2f0ae9ef14
Marco Pivetta <ocramius@gmail.com>
2014-12-12 17:20:28 +0100

    Applying hotfix: currently existing identifiers are used to check against the identity map during hydration of multiple hierarchical items referencing each other

--- lib/Doctrine/ODM/PHPCR/UnitOfWork.php
+++ lib/Doctrine/ODM/PHPCR/UnitOfWork.php
@@ -437,12 +437,13 @@ class UnitOfWork
         }
 
         foreach ($nodes as $node) {
-            $id = $node->getPath();
-            if (!isset($documents[$id])) {
+            $id       = $node->getPath();
+            $document = $this->getDocumentById($id) ?: (isset($documents[$id]) ? $documents[$id] : null);
+
+            if (! $document) {
                 continue;
             }
 
-            $document = $documents[$id];
             $class = $this->dm->getClassMetadata(get_class($document));
 
             $documentState = array();
