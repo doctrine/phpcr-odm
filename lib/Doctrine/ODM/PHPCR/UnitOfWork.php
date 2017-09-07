88aa68a6f827062a2c4afeb4ce47376e353c5322
David Buchmann <david@liip.ch>
2017-09-07 14:11:13 +0000

    Apply fixes from StyleCI

--- lib/Doctrine/ODM/PHPCR/UnitOfWork.php
+++ lib/Doctrine/ODM/PHPCR/UnitOfWork.php
@@ -2301,13 +2301,13 @@ class UnitOfWork
         $order = array_flip(array_values($oids));
         uasort($oids, function ($a, $b) use ($order) {
             // compute the node depths
-                $aCount = substr_count($a, '/');
+            $aCount = substr_count($a, '/');
             $bCount = substr_count($b, '/');
 
-                // ensure that the original order is maintained for nodes with the same depth
-                if ($aCount === $bCount) {
-                    return ($order[$a] < $order[$b]) ? -1 : 1;
-                }
+            // ensure that the original order is maintained for nodes with the same depth
+            if ($aCount === $bCount) {
+                return ($order[$a] < $order[$b]) ? -1 : 1;
+            }
 
             return ($aCount < $bCount) ? -1 : 1;
         }
