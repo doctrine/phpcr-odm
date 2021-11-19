a5a4edfefde1409493e28ee4f088b73f66fcfe28
Jordi Sala Morales <jordism91@gmail.com>
2021-11-19 08:56:10 +0100

    Add mysql service

--- tests/script_doctrine_dbal.sh
+++ tests/script_doctrine_dbal.sh
@@ -13,8 +13,6 @@ elif test "${SCRIPT_DIR:0:1}" != "/" ; then
   SCRIPT_DIR="$PWD/$SCRIPT_DIR"
 fi
 
-mysql -e 'create database IF NOT EXISTS phpcr_odm_tests;' -u root
-
 cp ${SCRIPT_DIR}/../cli-config.doctrine_dbal.php.dist ${SCRIPT_DIR}/../cli-config.php
 ${SCRIPT_DIR}/../bin/phpcrodm jackalope:init:dbal --force
 ${SCRIPT_DIR}/../bin/phpcrodm doctrine:phpcr:register-system-node-types
