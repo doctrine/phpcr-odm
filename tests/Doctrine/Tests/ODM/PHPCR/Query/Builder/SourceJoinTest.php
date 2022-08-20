6616ab95de677069e38af9135adbd1bfc00697af
David Buchmann <david.buchmann@liip.ch>
2022-08-20 22:24:30 +0200

    ops

--- tests/Doctrine/Tests/ODM/PHPCR/Query/Builder/SourceJoinTest.php
+++ tests/Doctrine/Tests/ODM/PHPCR/Query/Builder/SourceJoinTest.php
@@ -2,16 +2,18 @@
 
 namespace Doctrine\Tests\ODM\PHPCR\Query\Builder;
 
+use PHPCR\NodeInterface;
+
 class SourceJoinTest extends NodeTestCase
 {
-    public function getNode($args = [])
+    public function getNode($args = []): NodeInterface
     {
         $args[] = 'test-join-type';
 
         return parent::getNode($args);
     }
 
-    public function provideInterface()
+    public function provideInterface(): array
     {
         return [
             ['left', 'SourceJoinLeft', [
